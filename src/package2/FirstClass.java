6ff07547893d429fb5ae446c5b589d8331e547b8
Emrullah <emrullahd69@gmail.com>
2020-08-09 19:10:07 +0300

    this is the fifth commit from local master

--- src/package2/FirstClass.java
+++ src/package2/FirstClass.java
@@ -8,13 +8,12 @@ public class FirstClass {
 		System.out.println("This is second commit from local master");
 		System.out.println("This is the third commit from local master");
 		System.out.println("This is the first commit from remote repository");
-<<<<<<< HEAD
+
 		System.out.println("This is the fourth commit from local master");
-=======
 		
 		System.out.println("This is the second commit from remote master");
 		
->>>>>>> d5a6cf3f8616f9928cf945555c56068d47add09b
+
 	}
 
 }
